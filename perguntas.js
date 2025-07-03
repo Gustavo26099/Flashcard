cc5ea97c24d905bab24b495c3d924cdb2da8a34d
Gustavo26099 <161737236+Gustavo26099@users.noreply.github.com>
2025-07-03 07:50:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual seleção venceu mais copas do mundo de futebol?',
+    'Futebol com 5 titulos (1958, 1962, 1970,1994 e 2002)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o mair país do mundo em extensão territorial?',
+    'Rússia com aproximadamente 17 milhoes de km'
+)
+
+criaCartao(
+    'Volei',
+    'em que país o volei foi criado?',
+    'Estados Unidos (EUA)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
